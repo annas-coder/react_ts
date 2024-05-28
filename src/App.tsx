b4814a698fec40d810a1358faf48683c09790002
annas-coder <127987556+annas-coder@users.noreply.github.com>
2024-05-28 15:48:38 +0400

    updating tailwind styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 const App: React.FC = (): JSX.Element => {
   return (
-    <>
-      <div>
-        <h3>
-          will continue later 
-        </h3>
-      </div>
-    </>
+    <div className='bg-grey-500 w-full h-screen bg-gray-300 flex'>
+          <div className='grid grid-cols-2 gap-5 m-auto'>
+              <div className='bg-gray-200 w-full h-52'></div>
+              <div className='bg-gray-600 w-full'></div> 
+          </div>
+    </div>
   ); 
 };
 
